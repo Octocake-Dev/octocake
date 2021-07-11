17666a1d0ca634c7d08c55bf612b00b25eaddc97
imad <imadatyatalah@gmail.com>
2021-07-11 09:58:08 +0100

    feat(client): Added custom error 404 page

---
+++ client/src/pages/404.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Custom404 = () => {
+  return (
+    <>
+      <section>
+        <h1>404 - Page Not Found</h1>
+      </section>
+    </>
+  );
+};
+
+export default Custom404;
