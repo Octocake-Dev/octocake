f81e09740702f6bd001249229ff50120c2a6473b
imad <imadatyatalah@gmail.com>
2021-07-13 20:18:15 +0100

    feat(client): Added home page title

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
+import { NextSeo } from "next-seo";
+
 const Home = () => {
   return (
     <>
+      <NextSeo title="Home - Octocake" />
+
       <section></section>
     </>
   );
