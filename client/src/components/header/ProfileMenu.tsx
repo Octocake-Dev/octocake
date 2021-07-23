86231b8c6e68d4625660c15c1ed5f521245fef85
imad <imadatyatalah@gmail.com>
2021-07-23 18:27:06 +0100

    fix: Fix menu items z-index

--- client/src/components/header/ProfileMenu.tsx
+++ client/src/components/header/ProfileMenu.tsx
@@ -38,7 +38,7 @@ const ProfileMenu = ({ user }: { user: User }) => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="z-50 absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="px-1 py-1">
               <Link href={`/u/${user.githubUsername}`}>
                 <a>
