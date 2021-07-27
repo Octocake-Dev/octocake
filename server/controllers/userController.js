cfb60f00bfe8421de5fa87aeb2d05a9a4db51ea1
imad <imadatyatalah@gmail.com>
2021-07-27 19:56:16 +0100

    fix(server): Return user with posts not just posts #28

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,11 +2,10 @@ import { prisma } from "../config/prisma.js";
 
 export const getUser = async (req, res) => {
   try {
-    const user = await prisma.user
-      .findUnique({
-        where: { githubUsername: req.params.username },
-      })
-      .posts();
+    const user = await prisma.user.findUnique({
+      where: { githubUsername: req.params.username },
+      include: { posts: true },
+    });
 
     res.status(200).send(user);
   } catch (err) {
