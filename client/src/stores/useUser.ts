2e1108751173a8f4807cac4cdf2067d1a77b4500
imad <imadatyatalah@gmail.com>
2021-07-30 11:08:23 +0100

    fix(client): Move everything inside try and catch

--- client/src/stores/useUser.ts
+++ client/src/stores/useUser.ts
@@ -1,16 +1,16 @@
-import { combine } from "zustand/middleware";
 import create from "zustand";
+import { combine } from "zustand/middleware";
 
 import { instance } from "@/lib/axios";
 
 export const useUser = create(
   combine({ user: null, logged_in: false, oc_token: null }, (set) => ({
     fetchUser: async () => {
-      const { data: oc_token } = await instance.get("/oc_token", {
-        withCredentials: true,
-      });
-
       try {
+        const { data: oc_token } = await instance.get("/oc_token", {
+          withCredentials: true,
+        });
+
         const { data: user } = await instance.get("/current_user", {
           headers: { oc_token },
         });
