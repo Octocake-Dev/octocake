91861c67be3e27ad4f30612b71b327b56b88f166
imad <imadatyatalah@gmail.com>
2021-07-31 13:08:30 +0100

    feat(client): Use EsLint with TS

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -8,11 +8,12 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next",
   ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
+    ecmaFeatures: { jsx: true },
     ecmaVersion: 12,
     sourceType: "module",
   },
@@ -23,7 +24,7 @@ module.exports = {
       version: "detect",
     },
   },
-  plugins: ["react", "react-hooks"],
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": [
