b3fbac0650e8900aac2333e07a84395d31814c3e
imad <imadatyatalah@gmail.com>
2021-08-05 11:02:10 +0100

    feat(client): Added apiBaseUrl constant

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,3 +1,5 @@
 import axios from "axios";
 
-export const instance = axios.create({ baseURL: "http://localhost:1337" });
+import { apiBaseUrl } from "./constants";
+
+export const instance = axios.create({ baseURL: apiBaseUrl });
