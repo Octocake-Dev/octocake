0e7ae2296521aca9ea1e5d736162b90e7ed38925
imad <imadatyatalah@gmail.com>
2021-08-05 12:43:54 +0100

    run migrations

--- server/prisma/migrations/20210805113219_init/migration.sql
+++ server/prisma/migrations/20210805113219_init/migration.sql
@@ -10,6 +10,19 @@ CREATE TABLE "users" (
     "updated_at" DATETIME NOT NULL
 );
 
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    FOREIGN KEY ("ownerId") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users.github_email_unique" ON "users"("github_email");
 
@@ -18,3 +31,6 @@ CREATE UNIQUE INDEX "users.github_username_unique" ON "users"("github_username")
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users.github_id_unique" ON "users"("github_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "posts.slug_unique" ON "posts"("slug");
