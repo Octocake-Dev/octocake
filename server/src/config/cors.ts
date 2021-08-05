823a7215d0e43cc5e39bc95eb82787548812f6fe
imad <imadatyatalah@gmail.com>
2021-08-05 18:08:23 +0100

    feat(server): Added CLIENT_BASE_URL env var

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -1,6 +1,8 @@
+import { client_base_url } from "./credentials";
+
 export const corsOptions = {
   methods: ["GET", "PUT", "POST", "DELETE"],
-  origin: "http://localhost:3000",
+  origin: client_base_url,
   optionsSuccessStatus: 200,
   credentials: true,
 };
--- server/src/config/credentials.ts
+++ server/src/config/credentials.ts
@@ -6,3 +6,5 @@ export const jwt_key = process.env.JWT_KEY;
 export const server_port = process.env.PORT || 3000;
 export const github_client_id = process.env.GITHUB_ID;
 export const github_client_secret = process.env.GITHUB_SECRET;
+export const client_base_url =
+  process.env.CLIENT_BASE_URL || "http://localhost:3000";
