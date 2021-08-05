4db3fdee54cc655d8bec44b7b792e1acf067de52
imad <imadatyatalah@gmail.com>
2021-08-05 17:47:48 +0100

    fix(server): Added fallback port

--- server/src/config/credentials.ts
+++ server/src/config/credentials.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const jwt_key = process.env.JWT_KEY;
-export const server_port = process.env.PORT;
+export const server_port = process.env.PORT || 3000;
 export const github_client_id = process.env.GITHUB_ID;
 export const github_client_secret = process.env.GITHUB_SECRET;
