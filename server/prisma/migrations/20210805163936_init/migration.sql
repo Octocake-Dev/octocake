d488769155133b86d386d72d896cbd6f8b91ac7a
imad <imadatyatalah@gmail.com>
2021-08-05 17:40:17 +0100

    Use postgresql db

--- server/prisma/migrations/20210805163936_init/migration.sql
+++ server/prisma/migrations/20210805163936_init/migration.sql
@@ -1,26 +1,29 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "github_avatar_url" TEXT,
     "github_name" TEXT NOT NULL,
     "github_email" TEXT,
     "github_username" TEXT NOT NULL,
     "github_id" INTEGER NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME NOT NULL
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "posts" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
     "published" BOOLEAN NOT NULL,
     "ownerId" INTEGER NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME NOT NULL,
-    FOREIGN KEY ("ownerId") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -34,3 +37,6 @@ CREATE UNIQUE INDEX "users.github_id_unique" ON "users"("github_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "posts.slug_unique" ON "posts"("slug");
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD FOREIGN KEY ("ownerId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
