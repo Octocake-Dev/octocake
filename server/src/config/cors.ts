489a3c248b4e5bdf0c46ca174cf290619e9dc868
imad <imadatyatalah@gmail.com>
2021-08-06 12:54:04 +0100

    use allowedHeaders

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -2,6 +2,7 @@ import { client_base_url } from "./credentials";
 
 export const corsOptions = {
   methods: ["GET", "PUT", "POST", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   origin: client_base_url,
   optionsSuccessStatus: 200,
   credentials: true,
