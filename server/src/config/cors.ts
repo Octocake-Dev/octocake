a47eaa8cd8265da7d744cf9f1854ca582af8ad5d
imad <imadatyatalah@gmail.com>
2021-08-06 12:16:02 +0100

    enable cors just for client_base_url

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -2,7 +2,7 @@ import { client_base_url } from "./credentials";
 
 export const corsOptions = {
   methods: ["GET", "PUT", "POST", "DELETE"],
-  origin: "*",
+  origin: client_base_url,
   optionsSuccessStatus: 200,
   credentials: true,
 };
