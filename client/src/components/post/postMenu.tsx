8eb5a4f1925d5bc3799de2f1f0d9aae2cedb48dc
imad <imadatyatalah@gmail.com>
2021-08-09 09:33:11 +0100

    feat(client): Added copy to clipboard menu-item

--- client/src/components/post/postMenu.tsx
+++ client/src/components/post/postMenu.tsx
@@ -5,18 +5,24 @@ import { Menu, Transition } from "@headlessui/react";
 import { HiOutlineTrash, HiOutlinePencilAlt } from "react-icons/hi";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { BiBlock } from "react-icons/bi";
-import { MdReport } from "react-icons/md";
+import { MdReport, MdContentCopy } from "react-icons/md";
+import useClipboard from "react-use-clipboard";
 
 import { useUser } from "@/stores/useUser";
 import useDeletePost from "@/hooks/useDeletePost";
 import MenuItem from "@/components/menuItem";
 
+import { baseUrl } from "@/lib/constants";
 import { TPost } from "@/types/post";
 
 const PostMenu = ({ post }: { post: TPost }) => {
   const user = useUser((state) => state.user);
 
   const { slug, owner } = post;
+
+  const [isCopied, setCopied] = useClipboard(`${baseUrl}/p/${slug}`, {
+    successDuration: 4000,
+  });
 
   const { mutate: deletePost } = useDeletePost(slug);
 
@@ -39,38 +45,47 @@ const PostMenu = ({ post }: { post: TPost }) => {
       >
         <Menu.Items className="z-50 absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="px-1 py-1">
-            {owner.githubId === user?.githubId ? (
-              <>
-                <Link href={`/p/${slug}/edit`}>
-                  <a>
-                    <MenuItem>
-                      <HiOutlinePencilAlt
-                        className="w-5 h-5 mr-1"
-                        aria-hidden="true"
-                      />
-                      Edit post
-                    </MenuItem>
-                  </a>
-                </Link>
+            <MenuItem disabled={isCopied} onClick={setCopied}>
+              <MdContentCopy className="w-5 h-5 mr-1" aria-hidden="true" />
+              {isCopied ? "Copied to Clipboard" : "Copy post URL"}
+            </MenuItem>
 
-                <MenuItem warning onClick={() => deletePost()}>
-                  <HiOutlineTrash className="w-5 h-5 mr-1" aria-hidden="true" />
-                  Delete Post
-                </MenuItem>
-              </>
-            ) : (
-              <>
-                <MenuItem warning>
-                  <BiBlock className="w-5 h-5 mr-1" aria-hidden="true" />
-                  Block @{owner.githubUsername}
-                </MenuItem>
+            {user &&
+              (owner.githubId === user?.githubId ? (
+                <>
+                  <Link href={`/p/${slug}/edit`}>
+                    <a>
+                      <MenuItem>
+                        <HiOutlinePencilAlt
+                          className="w-5 h-5 mr-1"
+                          aria-hidden="true"
+                        />
+                        Edit post
+                      </MenuItem>
+                    </a>
+                  </Link>
 
-                <MenuItem warning>
-                  <MdReport className="w-5 h-5 mr-1" aria-hidden="true" />
-                  Report Post
-                </MenuItem>
-              </>
-            )}
+                  <MenuItem warning onClick={() => deletePost()}>
+                    <HiOutlineTrash
+                      className="w-5 h-5 mr-1"
+                      aria-hidden="true"
+                    />
+                    Delete Post
+                  </MenuItem>
+                </>
+              ) : (
+                <>
+                  <MenuItem warning>
+                    <BiBlock className="w-5 h-5 mr-1" aria-hidden="true" />
+                    Block @{owner.githubUsername}
+                  </MenuItem>
+
+                  <MenuItem warning>
+                    <MdReport className="w-5 h-5 mr-1" aria-hidden="true" />
+                    Report Post
+                  </MenuItem>
+                </>
+              ))}
           </div>
         </Menu.Items>
       </Transition>
