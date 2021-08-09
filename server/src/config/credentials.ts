a854e69e821a22ca9688abe4d84f69d90f49afe6
imad <imadatyatalah@gmail.com>
2021-08-09 09:53:20 +0100

    fix(server): Changer fallBack port

--- server/src/config/credentials.ts
+++ server/src/config/credentials.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const jwt_key = process.env.JWT_KEY;
-export const server_port = process.env.PORT || 3000;
+export const server_port = process.env.PORT || 1337;
 export const github_client_id = process.env.GITHUB_ID;
 export const github_client_secret = process.env.GITHUB_SECRET;
 export const client_base_url =
