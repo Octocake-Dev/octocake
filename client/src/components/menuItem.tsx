d2018febea3d87d6ba31b32ced63d50a60de3e76
imad <imadatyatalah@gmail.com>
2021-08-09 09:32:18 +0100

    style(client): Added disabled prop

--- client/src/components/menuItem.tsx
+++ client/src/components/menuItem.tsx
@@ -3,13 +3,15 @@ import { Menu } from "@headlessui/react";
 type MenuItemProps = {
   children: React.ReactNode;
   warning?: boolean;
+  disabled?: boolean;
   onClick?: () => void;
 };
 
-const MenuItem = ({ children, warning, onClick }: MenuItemProps) => (
-  <Menu.Item onClick={onClick}>
+const MenuItem = ({ children, warning, onClick, disabled }: MenuItemProps) => (
+  <Menu.Item onClick={onClick} disabled={disabled}>
     {({ active }) => (
       <button
+        disabled={disabled}
         className={`${
           active
             ? warning
@@ -18,7 +20,7 @@ const MenuItem = ({ children, warning, onClick }: MenuItemProps) => (
             : warning
             ? "text-red-500"
             : "text-gray-900"
-        } group flex rounded-md items-center w-full px-2 py-2 text-sm font-medium`}
+        } group flex rounded-md items-center w-full px-2 py-2 text-sm font-medium disabled:opacity-50`}
       >
         {children}
       </button>
