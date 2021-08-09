fcbe61c1564e43e5e75dd7b61bd7ce872aa5574d
imad <imadatyatalah@gmail.com>
2021-08-09 10:57:24 +0100

    fix(server): Remove @ts-ignore

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from "express";
+import express, { Response } from "express";
 
+import { CustomRequest } from "../types/request";
 import { prisma } from "../config/prisma";
 import verifyToken from "../middlewares/verifyToken";
 
 const router = express.Router();
 
-router.get("/", verifyToken, async (req: Request, res: Response) => {
+router.get("/", verifyToken, async (req: CustomRequest, res: Response) => {
   try {
     const user = await prisma.user.findUnique({
-      // @ts-ignore
       where: { githubId: Number(req.user.id) },
     });
 
