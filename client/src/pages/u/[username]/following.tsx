dd775cb6e39d6e1db41ca1865c7058340ae1b6ba
imad <imadatyatalah@gmail.com>
2021-08-15 08:09:00 +0100

    feat(client): Show "Work In Progress!" instead of blank page

--- client/src/pages/u/[username]/following.tsx
+++ client/src/pages/u/[username]/following.tsx
@@ -9,7 +9,11 @@ const Following = () => {
     <>
       <NextSeo />
 
-      <section></section>
+      <section className="flex flex-col justify-center items-center space-y-5 min-h-screen">
+        <div>
+          <h1 className="text-3xl font-bold">Work In Progress!</h1>
+        </div>
+      </section>
     </>
   );
 };
