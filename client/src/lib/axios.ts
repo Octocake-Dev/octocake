887d23883f09f5893b2873653e4bbe8af4cb676d
imad <imadatyatalah@gmail.com>
2021-08-16 12:36:30 +0100

    style: Format via prettier & Remove comment

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,4 +2,7 @@ import axios from "axios";
 
 import { apiBaseUrl } from "./constants";
 
-export const instance = axios.create({ baseURL: apiBaseUrl, withCredentials: true, });
+export const instance = axios.create({
+  baseURL: apiBaseUrl,
+  withCredentials: true,
+});
