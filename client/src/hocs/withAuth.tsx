4f61d6ae2e36547ded5ca51a5c74cfa306fd6693
imad <imadatyatalah@gmail.com>
2021-08-20 11:43:36 +0100

    feat(client): Push non logged-in users to home page

--- client/src/hocs/withAuth.tsx
+++ client/src/hocs/withAuth.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
 import { useUser } from "@/stores/useUser";
 
 // TODO: Remove any
 const WithAuth = (WrappedComponent: any) =>
   function WithAuthWrapper(props: any) {
+    const { push } = useRouter();
     const logged_in = useUser((state) => state.logged_in);
+
+    useEffect(() => {
+      !logged_in && push("/");
+    }, [logged_in]);
 
     return logged_in ? (
       <WrappedComponent {...props} />
