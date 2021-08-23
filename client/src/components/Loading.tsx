8de3d433b2cbe7e29010ee29db52ada8329e5d41
imad <imadatyatalah@gmail.com>
2021-08-23 11:36:32 +0100

    feat(client): Added simple loading component

---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loading = () => (
+  <section className="flex flex-col justify-center items-center space-y-5 min-h-screen">
+    <h1 className="text-3xl font-bold">Loading...</h1>
+  </section>
+);
+
+export default Loading;
