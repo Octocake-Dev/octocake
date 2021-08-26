1160c797867a4bb5f36e7bcbe26d817ff2db265d
imad <imadatyatalah@gmail.com>
2021-08-26 16:28:37 +0100

    style(client): Added some styles

--- client/src/components/CustomToaster.tsx
+++ client/src/components/CustomToaster.tsx
@@ -3,7 +3,19 @@ import React from "react";
 import { Toaster } from "react-hot-toast";
 
 const CustomToaster = () => (
-  <Toaster position="bottom-center" toastOptions={{ duration: 4000 }} />
+  <Toaster
+    position="bottom-center"
+    toastOptions={{
+      duration: 4000,
+      style: { fontWeight: 600, maxWidth: "100%" },
+
+      success: {
+        style: { background: "var(--color-primary-900)", color: "#fff" },
+      },
+      error: {},
+      loading: {},
+    }}
+  />
 );
 
 export default CustomToaster;
