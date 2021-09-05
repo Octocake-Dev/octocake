fa7f630c5ad797dc00181da3fad892ad6d498843
imad <imadatyatalah@gmail.com>
2021-09-05 09:56:10 +0100

    fix(client-sitemap): Remove robotsTxtOptions option

--- client/next-sitemap.js
+++ client/next-sitemap.js
@@ -1,7 +1,4 @@
 module.exports = {
   siteUrl: "https://octocake.netlify.app",
   generateRobotsTxt: true,
-  robotsTxtOptions: {
-    additionalSitemaps: ["https://octocake.netlify.app/sitemap.xml"],
-  },
 };
