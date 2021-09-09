a2400b2375d3056e76172117d6844845f17a6bac
imad <imadatyatalah@gmail.com>
2021-09-09 12:38:17 +0100

    fix(client): Cache Inter font in dev environment

--- client/next-headers.js
+++ client/next-headers.js
@@ -11,6 +11,17 @@ async function headers() {
       source: "/(.*)",
       headers: securityHeaders,
     },
+    // Uncomment only if you want to cache Inter font on development environment
+
+    // {
+    //   source: "/fonts/inter-var-latin.woff2",
+    //   headers: [
+    //     {
+    //       key: "Cache-Control",
+    //       value: "public, max-age=31536000, immutable",
+    //     },
+    //   ],
+    // },
   ];
 }
 
