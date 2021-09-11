5712eb4ae0fcab1882e7e441618858da47110451
imad <imadatyatalah@gmail.com>
2021-09-11 11:01:41 +0100

    feat(client): Added 2 sidebars

--- client/src/modules/home/homePage.tsx
+++ client/src/modules/home/homePage.tsx
@@ -5,8 +5,14 @@ import Posts from "@/components/Posts";
 import { IPost } from "@/types/post";
 
 const HomePage = ({ posts }: { posts: IPost[] | undefined }) => (
-  <section>
+  <section className="md:grid md:gap-x-2 md:grid-cols-[0.75fr,2fr] lg:grid-cols-[0.75fr,2fr,0.75fr]">
+    <aside className="hidden text-center text-white bg-primary-500 md:block">
+      Octocake
+    </aside>
     <Posts posts={posts} />
+    <aside className="hidden text-center text-white bg-primary-500 lg:block">
+      Octocake
+    </aside>
   </section>
 );
 
