28ab8ab9454b2b421da07a5ce245dcfbab83f3cf
imad <imadatyatalah@gmail.com>
2021-09-12 14:56:48 +0100

    fix(client): Remove asides content & styles

--- client/src/modules/home/homePage.tsx
+++ client/src/modules/home/homePage.tsx
@@ -6,13 +6,9 @@ import { IPost } from "@/types/post";
 
 const HomePage = ({ posts }: { posts: IPost[] | undefined }) => (
   <section className="md:grid md:gap-x-2 md:grid-cols-[0.75fr,2fr] lg:grid-cols-[0.75fr,2fr,0.75fr]">
-    <aside className="hidden text-center text-white bg-primary-500 md:block">
-      Octocake
-    </aside>
+    <aside className="hidden md:block" />
     <Posts posts={posts} />
-    <aside className="hidden text-center text-white bg-primary-500 lg:block">
-      Octocake
-    </aside>
+    <aside className="hidden lg:block" />
   </section>
 );
 
