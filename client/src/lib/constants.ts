e96f3a7bf130f138554705ec03e0754cc95aeb31
imad <imadatyatalah@gmail.com>
2021-09-13 11:37:39 +0100

    refactor(client): Remove fallback baseUrl

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,3 +1,2 @@
 export const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-export const baseUrl =
-  process.env.NEXT_PUBLIC_BASE_URL || "https://octocake.netlify.app";
+export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
