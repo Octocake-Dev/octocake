4017e9a5173669be3264af08aba05202139d7c0e
imad <imadatyatalah@gmail.com>
2021-09-13 16:46:26 +0100

    fix(client-eslint): Use screen from react-testing-library

--- client/src/ui/button/__tests__/Button.test.tsx
+++ client/src/ui/button/__tests__/Button.test.tsx
@@ -3,15 +3,15 @@
  */
 import React from "react";
 
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Button from "@/ui/button/Button";
 
 describe("Button", () => {
   it("should render a button with Click me text", () => {
-    const { getByText } = render(<Button>Click me</Button>);
+    render(<Button>Click me</Button>);
 
-    const text = getByText("Click me");
+    const text = screen.getByText("Click me");
 
     expect(text).toBeVisible();
   });
