6ba250fd664d83a7800c0aefd7aecab4d424b491
imad <imadatyatalah@gmail.com>
2021-09-15 19:30:39 +0100

    feat(client-seo): Generate dynamic sitemaps for posts

--- client/next-sitemap.js
+++ client/next-sitemap.js
@@ -1,4 +1,8 @@
 module.exports = {
   siteUrl: "https://octocake.netlify.app",
   generateRobotsTxt: true,
+  exclude: ["/server-sitemap.xml"],
+  robotsTxtOptions: {
+    additionalSitemaps: ["https://octocake.netlify.app/server-sitemap.xml"],
+  },
 };
---
+++ client/src/pages/server-sitemap.xml/index.tsx
@@ -0,0 +1,32 @@
+import { GetServerSideProps } from "next";
+
+import { getServerSideSitemap } from "next-sitemap";
+
+import { instance } from "@/lib/axios";
+
+import { IPost } from "@/types/post";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { data: posts } = await instance.get<IPost[]>("/posts");
+
+  const fields: {
+    loc: string;
+    lastmod: string;
+    changefreq: string;
+    priority: number;
+  }[] = [];
+
+  posts.forEach((post) =>
+    fields.push({
+      loc: `https://octocake.netlify.app/p/${post.slug}`,
+      lastmod: new Date().toISOString(),
+      changefreq: "daily",
+      priority: 0.7,
+    })
+  );
+
+  return getServerSideSitemap(ctx, fields);
+};
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export default () => {};
