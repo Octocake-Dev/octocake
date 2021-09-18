030ea39578b927ab85948fdc73015f58e61cdbeb
imad <imadatyatalah@gmail.com>
2021-09-18 16:53:31 +0100

    feat(server): Added migrations #81

---
+++ server/prisma/migrations/20210918155218_add_more_user_data/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "bio" TEXT,
+ADD COLUMN     "github_url" TEXT,
+ADD COLUMN     "location" TEXT,
+ADD COLUMN     "medium_url" TEXT,
+ADD COLUMN     "stackOverflow_url" TEXT,
+ADD COLUMN     "twitter_url" TEXT,
+ADD COLUMN     "website_url" TEXT;
