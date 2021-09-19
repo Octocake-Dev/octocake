a503e7c92197514808dae7089d0938286504dbbd
imad <imadatyatalah@gmail.com>
2021-09-19 17:40:11 +0100

    Format file

--- client/next.config.js
+++ client/next.config.js
@@ -4,9 +4,7 @@
  * @type {import('next').NextConfig}
  **/
 module.exports = {
-  images: {
-    domains: ["avatars.githubusercontent.com"],
-  },
+  images: { domains: ["avatars.githubusercontent.com"] },
   redirects: require("./next-redirects"),
   headers: require("./next-headers"),
 };
