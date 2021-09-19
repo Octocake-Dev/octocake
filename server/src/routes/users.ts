8183aa45ac3c074a7db19e3b5631a4dd1164315f
imad <imadatyatalah@gmail.com>
2021-09-19 10:18:44 +0100

    feat(server): Added UpdateUser controller #159

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import {
   getUser,
+  UpdateUser,
   isFollowed,
   toggleFollow,
 } from "../controllers/users.controller";
@@ -11,8 +12,12 @@ const router = Router();
 
 router.get("/:username", getUser);
 
+// TODO: path should be "/:username"
+router.put("/:username/profile", verifyToken, UpdateUser);
+
 router.get("/:username/isFollowed", verifyToken, isFollowed);
 
+// TODO: path should be "/:username/toggleFollow"
 router.put("/:username", verifyToken, toggleFollow);
 
 export default router;
