da19d8e8a079407020a6de9e328326e05ca35ba3
imad <imadatyatalah@gmail.com>
2021-09-19 10:47:06 +0100

    feat(client): Show a toast onSuccess & onError #159

--- client/src/hooks/useUpdateUser.ts
+++ client/src/hooks/useUpdateUser.ts
@@ -1,4 +1,5 @@
 import { useMutation } from "react-query";
+import toast from "react-hot-toast";
 
 import { instance } from "@/lib/axios";
 
@@ -7,7 +8,17 @@ import { UserData } from "@/types/user";
 const useUpdateUser = (username: string) =>
   useMutation(
     async (data: UserData) =>
-      await instance.put(`/users/${username}/profile`, data)
+      await instance.put(`/users/${username}/profile`, data),
+    {
+      onSuccess: () => {
+        toast.success("Your profile was updated successfully!");
+      },
+      onError: (err) => {
+        if ("message" in (err as Error)) {
+          toast.error((err as Error).message);
+        }
+      },
+    }
   );
 
 export default useUpdateUser;
