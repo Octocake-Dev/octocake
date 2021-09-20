635470af1139ff97029c9595b44a20e1a78c1ffd
imad <imadatyatalah@gmail.com>
2021-09-20 09:40:54 +0100

    fix(client): Fix types

--- client/src/validations/post.ts
+++ client/src/validations/post.ts
@@ -1,6 +1,7 @@
 import * as yup from "yup";
 
-export const schema = yup.object().shape({
+export const schema = yup.object({
   title: yup.string().required("Title is required!"),
   description: yup.string().required("Description is required!"),
+  // body: yup.string().required("Body is required!"),
 });
