2c7e87b8cbbdae313aa2771bd6e133206060b356
imad <imadatyatalah@gmail.com>
2021-09-24 12:21:07 +0100

    feat: Seeding database #191

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,14 @@
  * @type {import('next').NextConfig}
  **/
 module.exports = {
-  images: { domains: ["avatars.githubusercontent.com"] },
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      // Uncomment only in development environment
+
+      // "cdn.fakercloud.com"
+    ],
+  },
   redirects: require("./next-redirects"),
   headers: require("./next-headers"),
 };
