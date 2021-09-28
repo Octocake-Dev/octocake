dcb61944687e5569602f2a35d942b2caad25915c
imad <imadatyatalah@gmail.com>
2021-09-28 09:38:30 +0100

    fix(client): Improve types

--- client/src/hooks/useCreatePost.ts
+++ client/src/hooks/useCreatePost.ts
@@ -5,10 +5,10 @@ import toast from "react-hot-toast";
 
 import { instance } from "@/lib/axios";
 
-import { PostData } from "@/types/post";
+import { IPost, PostData } from "@/types/post";
 
 const useCreatePost = () =>
-  useMutation((data: PostData) => instance.post(`/posts`, data), {
+  useMutation((data: PostData) => instance.post<IPost>(`/posts`, data), {
     onSuccess: ({ data }) => {
       Router.push(`/p/${data.slug}`);
       toast.success("Your post was created successfully!");
