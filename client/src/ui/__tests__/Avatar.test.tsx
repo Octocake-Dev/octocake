814d43b7ffba0bf45f021b200aea25fc04725cb9
imad <imadatyatalah@gmail.com>
2021-10-07 21:46:47 +0100

    test(client): Set a global Avatar component

--- client/src/ui/__tests__/Avatar.test.tsx
+++ client/src/ui/__tests__/Avatar.test.tsx
@@ -7,14 +7,16 @@ import Avatar from "@/ui/Avatar";
 describe("Avatar", () => {
   const alt = "avatar";
 
+  const Component = <Avatar src="/logo.svg" width="50" height="50" alt={alt} />;
+
   it("Should render an avatar", () => {
-    render(<Avatar src="/logo.svg" width="50" height="50" alt={alt} />);
+    render(Component);
 
     expect(screen.getByAltText(alt)).toBeVisible();
   });
 
   it("Should match snapshot", () => {
-    render(<Avatar src="/logo.svg" width="50" height="50" alt={alt} />);
+    render(Component);
 
     expect(screen.getByAltText(alt)).toMatchSnapshot();
   });
