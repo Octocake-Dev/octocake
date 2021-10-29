fa2acd1f82981cd8dc82011f78b797f4f9429df6
imad <imadatyatalah@gmail.com>
2021-10-29 20:42:04 +0100

    fix: Fix ts err #246

--- client/src/utils/checkIsFollowed.ts
+++ client/src/utils/checkIsFollowed.ts
@@ -2,6 +2,6 @@ import type { ISimpleUser } from "@/types/user";
 
 export const checkIsFollowed = (
   // The type is not correct 100%, But it works fine(I'll fix that later).
-  followers: ISimpleUser[],
+  followers: ISimpleUser[] | undefined,
   currentUserId: number | undefined
 ) => followers?.filter((follower) => follower.id === currentUserId);
